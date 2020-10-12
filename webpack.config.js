0bed555e62656a5c34b7e91ecb937d60747c82eb
Kevin-Lai <kevinlai249@gmail.com>
2020-10-12 14:23:24 -0700

    Added initial frontend setup files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+module.exports = {
+    context: __dirname,
+
+    entry: "./frontend/query_arena.jsx",
+
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    
+    // give me line of code for errors
+    devtool: "source-map",
+    
+    resolve: {
+    
+        // if you do not add the "*",
+        // then it will add an extra .jsx to the filename.
+        // meaning: entry.jsx.jsx
+    
+        // makes importing easier
+        extensions: [".js", ".jsx", "*"]
+    },
+    
+    module: {
+        rules: [
+            {
+                //read all js and jsx files
+                test: /\.jsx?$/,
+                
+                // don't include what is specified
+                exclude: /(node_modules)/,
+                use: {
+                
+                    // tell webpack to use this to apply changes
+                    loader: "babel-loader",
+                    
+                    query: {
+                        // rules we set when webpack goes through the loader
+
+                        presets: ["@babel/env", "@babel/react"]
+                    }
+                },
+            }
+        ]
+    }
+};
