f598e8a507b6fa36f36e59ffdc8808ecd423990d
Kevin-Lai <kevinlai249@gmail.com>
2020-11-18 13:19:31 -0800

    Add answer reducer

--- frontend/reducers/answers_reducer.js
+++ frontend/reducers/answers_reducer.js
@@ -1,10 +1,20 @@
-
+import {
+    RECEIVE_ANSWER,
+    REMOVE_ANSWER
+} from '../actions/answer_actions';
 
 const AnswersReducer = (state = {}, action) => {
     Object.freeze(state);
     let newState = Object.assign({},state);
 
     switch (action.type) {
+
+        case RECEIVE_ANSWER:
+            newState[action.answer.id] = action.answer;
+            return newState;
+        case REMOVE_ANSWER:
+            delete newState[action.answerId];
+            return newState;
 
         default:
             return state;
