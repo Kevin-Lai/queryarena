758db2869a32a683da6dbdb062029afdd27a087c
Kevin-Lai <kevinlai249@gmail.com>
2021-02-14 19:26:27 -0800

    Add form component for edit question

--- frontend/components/questions/question_edit.jsx
+++ frontend/components/questions/question_edit.jsx
@@ -9,6 +9,18 @@ class QuestionEdit extends React.Component{
         return (
             <div className="question-form">
                 <button className="question-create-x" onClick={() => this.props.closeModal()}>X</button>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input className="question-body-input-box" placeholder={"Start your question with \"What\", \"How\", \"Why\", etc."} type="text" value={this.state.body} onChange={this.handleChange("body")}></input>
+                    <div className="question-create-buttons-block">
+                        <label></label>
+                        <div className="question-create-buttons">
+                            <button className="question-create-cancel-button" onClick={() => this.props.closeModal()}>Cancel</button>
+                            <button className="question-create-submit-button">Save</button>
+                        </div>
+                    </div>
+                </form>
+
             </div>
         )
     }
