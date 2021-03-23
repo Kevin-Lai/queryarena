54d2a944f2ef170030614d328e55bdddd5d2e08b
Kevin-Lai <kevinlai249@gmail.com>
2021-03-22 17:37:24 -0700

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -148,6 +148,7 @@ q49 = Question.create(user_id: creator.id, body: "What are the best mind bending
 q50 = Question.create(user_id: creator.id, body: "Are electrons smaller than atoms?")
 q51 = Question.create(user_id: creator.id, body: "What are electrons?")
 q52 = Question.create(user_id: creator.id, body: "What are protons?")
+q53 = Question.create(user_id: creator.id, body: "What is an atom?")
 
 Answer.create(user_id: u10.id, question_id: q1.id, body: "QueryArena, a Quora clone, is a web application that allows users to ask questions and share answers.")
 Answer.create(user_id: u1.id, question_id: q2.id, body: "There are 60 seconds in a minute.")
