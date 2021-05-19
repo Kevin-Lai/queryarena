f3935126f89654551ade86b2ab1e5d4e170eda8d
Kevin-Lai <kevinlai249@gmail.com>
2021-05-18 20:45:12 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -168,6 +168,7 @@ q69 = Question.create(user_id: creator.id, body: "What do you call animals that 
 q70 = Question.create(user_id: creator.id, body: "What do you call animals that only eat meat?")
 q71 = Question.create(user_id: creator.id, body: "How many teeth does an adult human have?")
 q72 = Question.create(user_id: creator.id, body: "What is the hottest planet in the solar system?")
+q73 = Question.create(user_id: creator.id, body: "H2SO4 is the chemical formula of which acid?")
 
 Answer.create(user_id: u10.id, question_id: q1.id, body: "QueryArena, a Quora clone, is a web application that allows users to ask questions and share answers.")
 Answer.create(user_id: u1.id, question_id: q2.id, body: "There are 60 seconds in a minute.")
