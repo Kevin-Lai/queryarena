94f922919010aaf034dcb334ca290e82d5fe3598
Kevin-Lai <kevinlai249@gmail.com>
2021-06-12 16:17:55 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -184,6 +184,7 @@ q85 = Question.create(user_id: creator.id, body: "What are animals, which eat bo
 q86 = Question.create(user_id: creator.id, body: "What is the closest planet to the sun?")
 q87 = Question.create(user_id: creator.id, body: "Which organelle is also called the powerhouse of the cell?")
 q88 = Question.create(user_id: creator.id, body: "What is the standard unit of measurement for force?")
+q89 = Question.create(user_id: creator.id, body: "What is the lightest element in the periodic table?")
 
 Answer.create(user_id: u10.id, question_id: q1.id, body: "QueryArena, a Quora clone, is a web application that allows users to ask questions and share answers.")
 Answer.create(user_id: u1.id, question_id: q2.id, body: "There are 60 seconds in a minute.")
