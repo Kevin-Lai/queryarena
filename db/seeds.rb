17e4d39a4a2a3ceb11750b925773fdb5cb29db6a
Kevin-Lai <kevinlai249@gmail.com>
2021-06-11 20:33:40 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -183,6 +183,7 @@ q84 = Question.create(user_id: creator.id, body: "What is the standard unit of m
 q85 = Question.create(user_id: creator.id, body: "What are animals, which eat both plants and animals, called?")
 q86 = Question.create(user_id: creator.id, body: "What is the closest planet to the sun?")
 q87 = Question.create(user_id: creator.id, body: "Which organelle is also called the powerhouse of the cell?")
+q88 = Question.create(user_id: creator.id, body: "What is the standard unit of measurement for force?")
 
 Answer.create(user_id: u10.id, question_id: q1.id, body: "QueryArena, a Quora clone, is a web application that allows users to ask questions and share answers.")
 Answer.create(user_id: u1.id, question_id: q2.id, body: "There are 60 seconds in a minute.")
