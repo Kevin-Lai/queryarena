9f585b1e022f1c546f8d7d2cfb3fba78f84153b5
Kevin-Lai <kevinlai249@gmail.com>
2021-07-28 19:19:48 -0700

    Update upvote model

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: upvotes
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  answer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Upvote < ApplicationRecord
+
+    # for Rails 5, when you write a belongs_to, it auto validates the presence of that foreign key
+
+    validates :user_id, :answer_id, presence: true
+    validates :answer_id, uniqueness: {scope: user_id}
+
+    belongs_to :user,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to :answer,
+        foreign_key: :answer_id,
+        class_name: :Answer
+
 end
