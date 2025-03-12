b2acf80e7f9828c8eefbe03a780f1f3b2f274c5e
Kevin-Lai <kevinlai247@gmail.com>
2025-03-11 23:06:02 -0700

    Update render commands

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,9 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Build
-npm install && npm run build
-
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
