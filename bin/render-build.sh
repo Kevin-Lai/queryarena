e287ba89bdd1c27187725801db73fad46d4dd755
Kevin-Lai <kevinlai247@gmail.com>
2025-03-18 21:36:39 -0700

    Update configurations

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,16 @@
 # exit on error
 set -o errexit
 
+# Build commands for the backend
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
+# Build commands for the frontend
+rm -rf public
+npm install && npm run build
+cp -a build/. public/
 
+# Seed the db
 bundle exec rails db:migrate
+bundle exec rails db:seed
